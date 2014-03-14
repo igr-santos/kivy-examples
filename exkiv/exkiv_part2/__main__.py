a677d6e4cf22e11bd388366ce29ee0601b3944fb
root <root@svnteste.(none)>
2014-03-14 14:42:42 -0300

    part 2 example

---
+++ exkiv/exkiv_part2/__main__.py
@@ -0,0 +1,16 @@
+#!/usr/lib/python
+
+import kivy
+kivy.require('1.0.6')
+
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+
+
+class AccountDetailsForm(BoxLayout):
+    pass
+
+class Exkiv(App):
+    pass
+
+Exkiv().run()
